b8c8c30b6bf083977d3500bbae723baf8b8cede3
Juan Diego García Nieto <juand.garcian@uqvirtual.edu.co>
2026-05-10 11:14:29 -0500

    fix: Faltaba activar el monitoreo de Spring Boot

--- recetas-app/src/main/java/com/recetas/app/config/SecurityConfig.java
+++ recetas-app/src/main/java/com/recetas/app/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/recetas").permitAll()
                         .requestMatchers("/api/recetas/buscar/**").permitAll()
